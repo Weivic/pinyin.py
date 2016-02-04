2650388d3ab25e129aef7c99567e74bc0f7ce4c2
Weivic <weivic@yeah.net>
2016-02-04 21:16:06 +0800

    更改类方

--- pinyin.py
+++ pinyin.py
@@ -2,60 +2,89 @@
 # -*- coding:utf-8 -*-
 
 """
-    Author:cleverdeng
-    E-mail:clverdeng@gmail.com
+    Author:WeiVic
 """
+import os.path
 
 __version__ = '0.9'
 __all__ = ["PinYin"]
 
-import os.path
-
 
 class PinYin(object):
-    def __init__(self, dict_file='word.data'):
-        self.word_dict = {}
-        self.dict_file = dict_file
+    word_dict = {}
+    word = None
+    __vowels__ = 'aeiouv'
+    __pinyin_dict__ = {
+        'a': 'āáǎàa',
+        'o': 'ōóǒòo',
+        'e': 'ēéěèe',
+        'i': 'īíǐìi',
+        'u': 'ūúǔùu',
+        'v': 'ǖǘǚǜü'
+    }
 
+    def __init__(self, word):
+        self.word = word
 
-    def load_word(self):
-        if not os.path.exists(self.dict_file):
-            raise IOError("NotFoundFile")
+    def __new__(cls, word):
+        cls.word = word
+        return object.__new__(PinYin)
 
-        with file(self.dict_file) as f_obj:
+    # @property
+    # def vowels(self):
+    #     return "aeiouv"
+
+    @classmethod
+    def load_word(cls, dict_file=None):
+        if dict_file is None:
+            dict_file = os.path.join(os.path.dirname(__file__), 'word.data')
+        if not os.path.exists(dict_file):
+            raise IOError("Data file not found!")
+
+        with open(dict_file) as f_obj:
             for f_line in f_obj.readlines():
-                try:
-                    line = f_line.split('    ')
-                    self.word_dict[line[0]] = line[1]
-                except:
-                    line = f_line.split('   ')
-                    self.word_dict[line[0]] = line[1]
+                line = f_line.lower().split()
+                key = line[0]
+                value_raw = line[1:]
+                value_pinyin = []
+                value_without_number = []
+                for py_raw in value_raw:
+                    py_pinyin = py_raw
+                    for vowel in cls.__vowels__:
+                        if vowel in py_raw:
+                            py_pinyin = py_raw.replace(vowel, cls.__pinyin_dict__[vowel][int(py_raw[-1])-1])[:-1]
+                            break
+                    value_pinyin.append(py_pinyin)
+                    value_without_number.append(py_raw[:-1])
+                cls.word_dict[key] = [value_raw, value_pinyin, value_without_number]
 
-
-    def hanzi2pinyin(self, string=""):
+    @classmethod
+    def convert(cls, convert_string=None, tone=True, num=False):
+        if convert_string is None:
+            if cls.word is None:
+                raise TypeError("Expect convert string!")
+            else:
+                convert_string = cls.word
         result = []
-        if not isinstance(string, unicode):
-            string = string.decode("utf-8")
-        
-        for char in string:
+        for char in convert_string:
             key = '%X' % ord(char)
-            result.append(self.word_dict.get(key, char).split()[0][:-1].lower())
-
+            result.append(cls.word_dict.get(key.lower(), char)[tone and 1 or (not num and 2 or 0)])
         return result
 
-
-    def hanzi2pinyin_split(self, string="", split=""):
-        result = self.hanzi2pinyin(string=string)
-        if split == "":
-            return result
-        else:
-            return split.join(result)
-
+    @classmethod
+    def convert_join(cls, convert_string=None, joiner="", tone=True, num=False):
+        result = cls.convert(convert_string, tone, num)
+        single_list = []
+        for item in result:
+            single_list.append(item[0])
+        return joiner.join(single_list)
 
 if __name__ == "__main__":
-    test = PinYin()
-    test.load_word()
-    string = "钓鱼岛是中国的"
-    print "in: %s" % string
-    print "out: %s" % str(test.hanzi2pinyin(string=string))
-    print "out: %s" % test.hanzi2pinyin_split(string=string, split="-")
+    # PinYin.load_word()
+    # string = "钓鱼岛是中国的"
+    # print("in: %s" % string)
+    # print("out: %s" % PinYin.convert_join(string, " ", False, False))
+    py = PinYin("钓鱼岛是中国的")
+    py.load_word()
+    print(py.convert_join(joiner=" "))
+
